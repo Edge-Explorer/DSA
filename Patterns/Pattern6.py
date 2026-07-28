0b92a955f33bd42205d413eb3ac14a36bf1aa17b
Karan Shelar <karanshelar8775@gmail.com>
2026-07-28 11:18:45 +0530

    solved pattern 6

---
+++ Patterns/Pattern6.py
@@ -0,0 +1,24 @@
+# Pattern 6
+
+# Given an integer n. 
+# You need to recreate the pattern given below for any value of N. 
+# Let's say for N = 5, the pattern should look like as below:
+
+# 12345
+
+# 1234
+
+# 123
+
+# 12
+
+# 1
+
+# Print the pattern in the function given to you.
+
+class Solution:
+    def pattern6(self, n):
+        for i in range(n):
+            for j in range(n, i, -1):
+                print(n-j+1, end=" ")
+            print()
