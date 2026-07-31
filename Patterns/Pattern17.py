90e2a1b5c7d9939086eda0b6b66cbb83e708a354
Karan Shelar <karanshelar8775@gmail.com>
2026-07-31 13:03:49 +0530

    solved pattern 17

---
+++ Patterns/Pattern17.py
@@ -0,0 +1,34 @@
+# Given an integer n. 
+# You need to recreate the pattern given below for any value of N. 
+# Let's say for N = 5, the pattern should look like as below:
+
+#     A
+#    ABA
+#   ABCBA
+#  ABCDCBA
+# ABCDEDCBA
+
+# Print the pattern in the function given to you.
+
+# Solution:
+
+class Solution:
+    def pattern17(self, n):
+        for i in range(n):
+            
+            # Print leading spaces
+            print(" " * (n - i - 1), end="")
+            
+            # Initialize character to start from 'A'
+            ch = ord('A')
+            breakpoint = (2 * i + 1) // 2
+            
+            # Print characters in row
+            for j in range(1, 2 * i + 2):
+                print(chr(ch), end="")
+                if j <= breakpoint:
+                    ch += 1
+                else:
+                    ch -= 1
+
+            print()
