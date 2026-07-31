9de6457804626ce017ef5e0414f87cfb7209b6ae
Karan Shelar <karanshelar8775@gmail.com>
2026-07-31 11:26:20 +0530

    solved pattern 14

---
+++ Patterns/Pattern14.py
@@ -0,0 +1,25 @@
+# Given an integer n. 
+# You need to recreate the pattern given below for any value of N. 
+# Let's say for N = 5, the pattern should look like as below:
+
+# A
+
+# AB
+
+# ABC
+
+# ABCD
+
+# ABCDE
+
+# Print the pattern in the function given to you.
+
+#Solution:
+
+class Solution:
+    def pattern14(self, n):
+        for i in range(n):
+            # Inner loop to print alphabets from A to A + i
+            for j in range(i+1):
+                print(chr(65+j), end="")   # Print the alphabet character followed by a space
+            print()
