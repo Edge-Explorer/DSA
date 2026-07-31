437349f778b1d17dc40dbe9bfaea658e097cbcef
Karan Shelar <karanshelar8775@gmail.com>
2026-07-31 11:39:38 +0530

    solved pattern 15

---
+++ Patterns/Pattern15.py
@@ -0,0 +1,24 @@
+# Given an integer n. 
+# You need to recreate the pattern given below for any value of N. 
+# Let's say for N = 5, the pattern should look like as below:
+
+# ABCDE
+
+# ABCD
+
+# ABC
+
+# AB
+
+# A
+
+# Print the pattern in the function given to you.
+
+# Solution:
+
+class Solution:
+    def pattern15(self, n):
+        for i in range(n):
+            for j in range(n-i):
+                print(chr(65+j), end="")
+            print()
